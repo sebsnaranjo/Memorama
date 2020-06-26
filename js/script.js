bb7d99c99829ff55ef56ce4ecca523e9d22d1ed3
Sebastian Naranjo <42097936+sebsnaranjo@users.noreply.github.com>
2020-06-26 16:19:26 -0500

    Fichas en pantalla

--- js/script.js
+++ js/script.js
@@ -23,14 +23,34 @@ class Memorama {
     eventos() {
         window.addEventListener('DOMContentLoaded', () => {
             this.cargaPantalla();
-        });
+        })
     }
 
-   
 
-    async cargaPantalla(){
-        const respuesta = await fetch('');
-        console.log(respuesta.json())
+    async cargaPantalla() {
+        const respuesta = await fetch('../memo.json');
+        const data = await respuesta.json();
+        this.totalTarjetas = data;
+        if (this.totalTarjetas.length > 0 ) {
+            this.totalTarjetas.sort(orden);
+            //organizacion de forma aleatoria
+            function orden(a,b) {
+                return Math.random() - 0.5;
+            }
+        }
+
+        this.numeroTarjetas = this.totalTarjetas.length;
+
+
+        let html = '';
+        this.totalTarjetas.forEach(card => {
+            html += `<div class="tarjeta">
+                        <img class="tajeta-img" src=${card.src} alt="imagen memorama">
+                    </div>`
+        })
+
+        this.$contenedorTarjetas.innerHTML = html;
+
     }
 }
 
